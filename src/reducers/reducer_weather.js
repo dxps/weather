27f45707ea36c1ebbd900c272f9d410d0d20d655
visvadw <visvadw@gmail.com>
2016-02-24 23:25:51 +0200

    Section 5, complete.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,15 @@
+import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
 
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // [ ] as multiple cities will be included
+            // concat() will return a new state (existing state won't be manipulated)
+            // it may be: return state.concat([ action.payload.data ]);
+            // or ES6 way of doing it:
+            return [ action.payload.data, ...state ];
+    }
     // console.log('WeatherReducer > Action received: ', action);
     return state;
 
